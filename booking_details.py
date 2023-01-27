8ed001c972fa415921434afb41ab5ccf6327d4d9
Sofiane <derraz.sofiane@gmail.com>
2023-01-27 17:03:07 +0100

    Réécriture des variables entités

--- booking_details.py
+++ booking_details.py
@@ -2,17 +2,42 @@
 # Licensed under the MIT License.
 
 
+# class BookingDetails:
+#     def __init__(
+#         self,
+#         destination: str = None,
+#         origin: str = None,
+#         travel_date: str = None,
+#         unsupported_airports=None,
+#     ):
+#         if unsupported_airports is None:
+#             unsupported_airports = []
+#         self.destination = destination
+#         self.origin = origin
+#         self.travel_date = travel_date
+#         self.unsupported_airports = unsupported_airports
+
+### BEGIN : Réadaptation des entités
 class BookingDetails:
     def __init__(
         self,
-        destination: str = None,
-        origin: str = None,
-        travel_date: str = None,
+        dst_city: str = None,
+        or_city: str = None,
+        str_date: str = None,
+        end_date: str = None,
+        budget: str = None,
+        n_adults: int = None,
+        n_children: int = None,
         unsupported_airports=None,
     ):
         if unsupported_airports is None:
             unsupported_airports = []
-        self.destination = destination
-        self.origin = origin
-        self.travel_date = travel_date
+        self.dst_city = dst_city
+        self.or_city = or_city
+        self.str_date = str_date
+        self.end_date = end_date
+        self.budget = budget
+        self.n_adults = n_adults
+        self.n_children = n_children
         self.unsupported_airports = unsupported_airports
+### END
