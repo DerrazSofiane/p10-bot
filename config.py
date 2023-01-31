20b8de374743052cf728f777db24f07ed91d61c6
Sofiane <derraz.sofiane@gmail.com>
2023-01-31 20:18:39 +0100

    Updated config.py

--- config.py
+++ config.py
@@ -4,21 +4,18 @@
 """Configuration for the bot."""
 
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 
 class DefaultConfig:
     """Configuration for the bot."""
 
     PORT = 3978
-    APP_ID = os.getenv("MicrosoftAppId", "")
-    APP_PASSWORD = os.getenv("MicrosoftAppPassword", "")
-    LUIS_APP_ID = os.getenv("LuisAppId", "")
-    LUIS_API_KEY = os.getenv("LuisAPIKey", "")
+    APP_ID = os.environ.get("MicrosoftAppId", "")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    LUIS_APP_ID = os.environ.get("LuisAppId", "")
+    LUIS_API_KEY = os.environ.get("LuisAPIKey", "")
     # LUIS endpoint host name, ie "westus.api.cognitive.microsoft.com"
-    LUIS_API_HOST_NAME = os.getenv("LuisAPIHostName", "")
-    APPINSIGHTS_INSTRUMENTATION_KEY = os.getenv(
+    LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName", "")
+    APPINSIGHTS_INSTRUMENTATION_KEY = os.environ.get(
         "AppInsightsInstrumentationKey", ""
     )
